5683ad07a67c4cd885e9852e8c154bf2605b7cb0
Minh Tu Le <minhtule05@gmail.com>
2015-02-23 22:49:25 +0700

    Init a framework project

---
+++ PredictionIOSDK/PredictionIOSDK.h
@@ -0,0 +1,19 @@
+//
+//  PredictionIOSDK.h
+//  PredictionIOSDK
+//
+//  Created by Minh Tu Le on 2/23/15.
+//  Copyright (c) 2015 PredictionIO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PredictionIOSDK.
+FOUNDATION_EXPORT double PredictionIOSDKVersionNumber;
+
+//! Project version string for PredictionIOSDK.
+FOUNDATION_EXPORT const unsigned char PredictionIOSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PredictionIOSDK/PublicHeader.h>
+
+
