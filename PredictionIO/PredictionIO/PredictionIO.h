4d9c2b679eed63bf1a1e0cba7ef28450b69254be
Minh-Tu Le <minhtule05@gmail.com>
2018-01-01 11:05:38 +0800

    Add skeleton workspace and SDK project

---
+++ PredictionIO/PredictionIO/PredictionIO.h
@@ -0,0 +1,19 @@
+//
+//  PredictionIO.h
+//  PredictionIO
+//
+//  Created by Minh Tu Le on 1/1/18.
+//  Copyright © 2018 Minh Tu Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PredictionIO.
+FOUNDATION_EXPORT double PredictionIOVersionNumber;
+
+//! Project version string for PredictionIO.
+FOUNDATION_EXPORT const unsigned char PredictionIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PredictionIO/PublicHeader.h>
+
+
